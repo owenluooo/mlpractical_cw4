686bd20fa98efb04db906d2011f8a1fd4b026940
owenluooo <shaonanowen@gmail.com>
2022-03-28 14:53:04 +0100

    Use vision transformer and has 98% accuracy but input data should be preprocessed aligned to the previous experiments

--- themodel.py
+++ themodel.py
@@ -19,6 +19,7 @@ from Net2 import *
 from MultiheadAttention import *
 from TransformerEncoderLayer import *
 from ConvBlocks import *
+from vit import *
 
 with h5py.File('all-samples-train-whole.h5', 'r') as f:
     for key in f.keys():
@@ -71,38 +72,89 @@ print(type(trainset))
 train_dataloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=2) # 取数据集
 eval_dataloader = torch.utils.data.DataLoader(evalset, batch_size=1, shuffle=True, num_workers=2)    
         
-
 # Initialisation
 # net = Net2()
 # net = ConvBlocks()
-net = Model(19).cuda()
-criterion = nn.CrossEntropyLoss()
+# net = Model(19).cuda()
+net = ViT(
+    image_size = 256,
+    patch_size = 16,
+    num_classes = 1000,
+    dim = 1024,
+    depth = 6,
+    heads = 16,
+    mlp_dim = 2048,
+    dropout = 0.1,
+    emb_dropout = 0.1
+).cuda()
+# criterion = nn.CrossEntropyLoss()
 
-optimiser = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
-for epoch in range(25): 
-    running_loss = 0.0
-    for i, (inputs, labels) in enumerate(train_dataloader, start=0):
+# optimiser = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
+# for epoch in range(25): 
+#     running_loss = 0.0
+#     for i, (inputs, labels) in enumerate(train_dataloader, start=0):
 
-        inputs = inputs.unsqueeze(1) # add one dimension # torch.Size([1, 1, 128, 22])
+#         inputs = inputs.unsqueeze(1) # add one dimension # torch.Size([1, 1, 128, 22])
+#         inputs = inputs[:, :, :, 0:16]
 
-        optimiser.zero_grad()
-        # forward + backward + optimize
-        outputs = net(inputs.cuda()) # torch.Size([1, 5])
-        _, labels = torch.max(labels, 1)
-        # print(outputs)
-        # print(labels)
-        loss = criterion(outputs, labels.cuda())
-        loss.backward()
-        optimiser.step()
+#         optimiser.zero_grad()
+#         # forward + backward + optimize
+#         outputs = net(inputs.cuda()) # torch.Size([1, 5])
+#         _, labels = torch.max(labels, 1)
+#         # print(outputs)
+#         # print(labels)
+#         loss = criterion(outputs, labels.cuda())
+#         loss.backward()
+#         optimiser.step()
 
+#         # print(f'iteration: ', i)
 
-        # print statistics
-        running_loss += loss.item()
-        if i == len(train_dataloader)-1:  # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 100))
-            running_loss = 0.0
-print('Finished Training')
-PATH = './wave_model_han16net.pth'
+#         # print statistics
+#         running_loss += loss.item()
+#         if i == len(train_dataloader) - 1:
+#             print('[%d, %5d] loss: %.3f' %
+#                   (epoch + 1, i + 1, running_loss / 100))
+#             running_loss = 0.0
+# print('Finished Training')
+# PATH = './wave_model_han16net.pth'
 # PATH = './wave_model_net.pth'
-torch.save(net.state_dict(), PATH)
+PATH = './wave_model_vit.pth'
+# torch.save(net.state_dict(), PATH)
+
+net.load_state_dict(torch.load(PATH))  # 将预训练的参数权重加载到新的模型之中
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(eval_dataloader, start=0):
+        inputs = inputs.unsqueeze(1) # add one dimension
+        inputs = inputs[:, :, :, 0:16]
+        inputs = inputs.to('cuda')
+        outputs = net(inputs)
+        newlabels = labels > 0
+        indices =  newlabels.nonzero()     
+        print(outputs.data)
+        pred_sum = outputs.data.sum()
+        _, pred_max = torch.max(outputs.data, 1)
+        pred_max = outputs.data[0][pred_max]
+        print(pred_sum)
+        print(pred_max)
+        pred_index = outputs.data / pred_max
+        print(1)
+        print(pred_index)
+        
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        print(indices)
+        correct += (predicted == indices[0][1]).sum().item()
+        print('label:')
+        print(labels)
+        print('newlabel:')
+        print(indices[0][1])
+        print('predicted:')
+        print(predicted)        
+        print('Total: %d' % total)
+        print('Correct: %d' % correct)
+        
+
+print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
